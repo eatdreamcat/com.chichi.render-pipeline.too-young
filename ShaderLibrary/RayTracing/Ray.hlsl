72892e98c7e2e8726d08a52c2fb9b19d35089ded
eatdreamcat <meow.std@outlook.com>
2024-04-25 16:35:50 +0800

    重命名

---
+++ ShaderLibrary/RayTracing/Ray.hlsl
@@ -0,0 +1,10 @@
+#ifndef TOO_YOUNG_RAY_INCLUDED
+#define TOO_YOUNG_RAY_INCLUDED
+
+struct Ray
+{
+    float3 origin;
+    float3 direction;
+};
+
+#endif
--- ShaderLibrary/ShaderVariables.hlsl
+++ ShaderLibrary/ShaderVariables.hlsl
@@ -1,5 +1,5 @@
-#ifndef UNITY_SHADER_VARIABLES_INCLUDED
-#define UNITY_SHADER_VARIABLES_INCLUDED
+#ifndef TOO_YOUNG_SHADER_VARIABLES_INCLUDED
+#define TOO_YOUNG_SHADER_VARIABLES_INCLUDED
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/GlobalSamplers.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/TextureXR.hlsl"
