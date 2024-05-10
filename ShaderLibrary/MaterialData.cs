eb277e52bd07db098f0d64ce1466a6b0454bfa73
eatdreamcat <meow.std@outlook.com>
2024-05-10 12:15:34 +0800

    0.0.2

---
+++ ShaderLibrary/MaterialData.cs
@@ -0,0 +1,24 @@
+namespace UnityEngine.Rendering.TooYoung
+{
+    [GenerateHLSL(generateCBuffer = true)]
+    public struct MaterialProperties
+    {
+        public Color albedoColor;
+
+        public float roughness;
+
+        public float metallic;
+        
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Mean_free_path
+        /// In physics, mean free path is the average distance over which a moving particle
+        /// (such as an atom, a molecule, or a photon) travels before substantially
+        /// changing its direction or energy (or, in a specific context, other properties),
+        /// typically as a result of one or more successive collisions with other particles.
+        /// </summary>
+        /// unit is nm，10^-9 m
+        public float meanFreePath;
+
+        public float density;
+    }
+}
---
+++ ShaderLibrary/MaterialData.cs.hlsl
@@ -0,0 +1,32 @@
+//
+// This file was automatically generated. Please don't edit by hand. Execute Editor command [ Edit > Rendering > Generate Shader Includes ] instead
+//
+
+#ifndef MATERIALDATA_CS_HLSL
+#define MATERIALDATA_CS_HLSL
+// Generated from UnityEngine.Rendering.TooYoung.MaterialProperties
+// PackingRules = Exact
+struct MaterialProperties
+{
+    float4 albedoColor; // x: r y: g z: b w: a 
+    float roughness;
+    float metallic;
+};
+
+//
+// Accessors for UnityEngine.Rendering.TooYoung.MaterialProperties
+//
+float4 GetAlbedoColor(MaterialProperties value)
+{
+    return value.albedoColor;
+}
+float GetRoughness(MaterialProperties value)
+{
+    return value.roughness;
+}
+float GetMetallic(MaterialProperties value)
+{
+    return value.metallic;
+}
+
+#endif
